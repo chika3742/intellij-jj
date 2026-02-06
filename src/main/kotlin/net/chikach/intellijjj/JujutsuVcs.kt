d08f24c70c46dc18fbda45bfa77d4cad543e6f01
chika3742 <kazu.chika.shima@gmail.com>
2026-02-06 09:01:19 +0900

    Implement VcsRootChecker

--- src/main/kotlin/net/chikach/intellijjj/JujutsuVcs.kt
+++ src/main/kotlin/net/chikach/intellijjj/JujutsuVcs.kt
@@ -3,8 +3,6 @@ package net.chikach.intellijjj
 import com.intellij.openapi.options.Configurable
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vcs.AbstractVcs
-import com.intellij.openapi.vcs.ProjectLevelVcsManager
-import com.intellij.openapi.vcs.VcsException
 import com.intellij.openapi.vcs.VcsKey
 import com.intellij.openapi.vcs.VcsType
 import com.intellij.openapi.vcs.changes.ChangeProvider
@@ -13,10 +11,8 @@ import com.intellij.openapi.vcs.diff.DiffProvider
 import com.intellij.openapi.vcs.history.VcsHistoryProvider
 import com.intellij.openapi.vcs.rollback.RollbackEnvironment
 import com.intellij.openapi.vcs.update.UpdateEnvironment
-import com.intellij.openapi.vfs.VirtualFile
 import net.chikach.intellijjj.commands.JujutsuCommandExecutor
 import net.chikach.intellijjj.diff.JujutsuDiffProvider
-import net.chikach.intellijjj.JujutsuChangeProvider
 
 class JujutsuVcs(project: Project) : AbstractVcs(project, "Jujutsu") {
     
@@ -33,12 +29,6 @@ class JujutsuVcs(project: Project) : AbstractVcs(project, "Jujutsu") {
     }
 
     override fun getChangeProvider(): ChangeProvider = changeProvider
-
-    override fun isVersionedDirectory(dir: VirtualFile?): Boolean {
-        if (dir == null || !dir.isDirectory) return false
-        val jjDir = dir.findChild(".jj")
-        return jjDir != null && jjDir.isDirectory
-    }
 
     override fun getCheckinEnvironment(): CheckinEnvironment? {
         return null // Jujutsu doesn't use traditional checkin
@@ -64,14 +54,8 @@ class JujutsuVcs(project: Project) : AbstractVcs(project, "Jujutsu") {
 
     companion object {
         const val VCS_NAME = "Jujutsu"
-        const val VCS_KEY = "Jujutsu"
         private val ourKey = createKey(VCS_NAME)
         
         fun getKey(): VcsKey = ourKey
-        
-        fun getInstance(project: Project): JujutsuVcs? {
-            return ProjectLevelVcsManager.getInstance(project)
-                .findVcsByName(VCS_NAME) as? JujutsuVcs
-        }
     }
 }
---
+++ src/main/kotlin/net/chikach/intellijjj/JujutsuVcsRootChecker.kt
@@ -0,0 +1,21 @@
+package net.chikach.intellijjj
+
+import com.intellij.openapi.vcs.VcsKey
+import com.intellij.openapi.vcs.VcsRootChecker
+import com.intellij.openapi.vfs.VirtualFile
+
+class JujutsuVcsRootChecker : VcsRootChecker() {
+    override fun getSupportedVcs(): VcsKey = JujutsuVcs.getKey()
+
+    override fun isRoot(file: VirtualFile): Boolean {
+        if (!file.isDirectory) return false
+        val jjDir = file.findChild(JUJUTSU_DIR)
+        return jjDir != null && jjDir.isDirectory
+    }
+
+    override fun isVcsDir(dirName: String): Boolean = dirName == JUJUTSU_DIR
+
+    companion object {
+        private const val JUJUTSU_DIR = ".jj"
+    }
+}
