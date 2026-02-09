5588f84f81e1e7097d3586937e0c355cebe6aae2
chika3742 <kazu.chika.shima@gmail.com>
2026-02-09 09:30:36 +0900

    Fix sometimes cannot parse JujutsuCommitRef

--- src/main/kotlin/net/chikach/intellijjj/jujutsu/models/JujutsuCommitRef.kt
+++ src/main/kotlin/net/chikach/intellijjj/jujutsu/models/JujutsuCommitRef.kt
@@ -1,13 +1,22 @@
+@file:OptIn(ExperimentalSerializationApi::class)
+
 package net.chikach.intellijjj.jujutsu.models
 
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.JsonIgnoreUnknownKeys
+import kotlinx.serialization.json.JsonNames
 
 /**
  * Bookmark/reference data embedded in `jj log` commit output.
  */
 @Serializable
+@JsonIgnoreUnknownKeys
 class JujutsuCommitRef(
     val name: String,
     val target: List<String>,
+    val remote: String? = null,
+    @JsonNames("tracking_target")
+    val trackingTarget: List<String>? = null,
 ) {
 }
